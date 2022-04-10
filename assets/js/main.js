0b25ed58b0d2391335069aa807a4e389dd23dddc
marizoo <marizoo@meow.woof>
2022-04-10 21:18:31 +0800

    edited nav-link and removed web3form

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,49 +145,6 @@ function scrollActive() {
 }
 window.addEventListener("scroll", scrollActive);
 
-/*=============== web3form ===============*/
-const form = document.getElementById("contact-form");
-const result = document.getElementById("result");
-
-form.addEventListener("submit", function (e) {
-    const formData = new FormData(form);
-    e.preventDefault();
-    var object = {};
-    formData.forEach((value, key) => {
-        object[key] = value;
-    });
-    var json = JSON.stringify(object);
-    result.innerHTML = "Please wait my darling...";
-
-    fetch("https://api.web3forms.com/submit", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-        },
-        body: json,
-    })
-        .then(async (response) => {
-            let json = await response.json();
-            if (response.status == 200) {
-                result.innerHTML = " 🥰 Yayyy! Thanks for your message.";
-            } else {
-                console.log(response);
-                result.innerHTML = json.message;
-            }
-        })
-        .catch((error) => {
-            console.log(error);
-            result.innerHTML = "☹️ Oopsies... Please try again.";
-        })
-        .then(function () {
-            form.reset();
-            setTimeout(() => {
-                result.style.display = "none";
-            }, 5000);
-        });
-});
-
 /*=============== VIDEO TOGGLE ===============*/
 function toggleVideo() {
     const trailer = document.querySelector(".play-video-container");
